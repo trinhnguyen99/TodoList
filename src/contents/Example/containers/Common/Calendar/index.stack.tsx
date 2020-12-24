93b4a156cc90deadacce9eef67f069b77f064328
truongquoc <ngotruongquoc0102@gmail.com>
2020-12-24 23:48:17 +0700

    Feat: feat synchronize with calendar

--- src/contents/Example/containers/Common/Calendar/index.stack.tsx
+++ src/contents/Example/containers/Common/Calendar/index.stack.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import CalenderApp from './screens/index';
-import calenderStack from './router';
+import AgendaApp from './screens/agendarScreen';
 
+import calenderStack from './router';
 
 const Stack = createStackNavigator();
 
 export default function CalenderAppStack() {
   return (
     <Stack.Navigator headerMode="none">
-      <Stack.Screen name={calenderStack.calender} component={CalenderApp} />
+      <Stack.Screen name={calenderStack.calender} component={AgendaApp} />
     </Stack.Navigator>
   );
 }
--- src/contents/Example/containers/Common/DiaryApp/screens/AddDiary.tsx
+++ src/contents/Example/containers/Common/DiaryApp/screens/AddDiary.tsx
@@ -17,11 +17,14 @@ import { ColorPicker } from 'react-native-color-picker';
 import firestore from '@react-native-firebase/firestore';
 import { lightPrimaryColor } from '@themes/ThemeComponent/Common/Color';
 import NavigationService from '@utils/navigation';
-
+import RNCalendarEvents from 'react-native-calendar-events';
+import { Alert } from 'react-native';
 interface Props {}
 interface State {
   color: string;
 }
+
+// RNCalendarEvents.requestPermissions((readonly: false))
 class AddDiary extends PureComponent<Props, State> {
   title: any;
 
@@ -29,29 +32,90 @@ class AddDiary extends PureComponent<Props, State> {
 
   datePickerRef: any;
 
-  timePickerRef: any;
+  starttimePickerRef: any;
+  endTimePickerRef: any;
 
   constructor(props: Props) {
     super(props);
     this.state = {
       color: lightPrimaryColor,
     };
+    // fs.readFile('credentials.json', (err, content: any) => {
+    //   if (err) return console.log('Error loading client secret file:', err);
+    //   // Authorize a client with credentials, then call the Google Calendar API.
+    //   this.authorize(JSON.parse(content), this.listEvents);
+    // });
+    RNCalendarEvents.requestPermissions(true).then(
+      (result) => {
+        Alert.alert('Read-only Auth requested', result);
+      },
+      (result) => {
+        console.error(result);
+      },
+    );
   }
+
+  // getAccessToken = (oAuth2Client: any, callback: any) => {
+  //   const authUrl = oAuth2Client.generateAuthUrl({
+  //     access_type: 'offline',
+  //     scope: SCOPES,
+  //   });
+  //   console.log('Authorize this app by visiting this url:', authUrl);
+  //   const rl = readline.createInterface({
+  //     input: process.stdin,
+  //     output: process.stdout,
+  //   });
+  //   rl.question('Enter the code from that page here: ', (code) => {
+  //     rl.close();
+  //     oAuth2Client.getToken(code, (err: any, token: any) => {
+  //       if (err) return console.error('Error retrieving access token', err);
+  //       oAuth2Client.setCredentials(token);
+  //       // Store the token to disk for later program executions
+  //       // fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+  //       //   if (err) return console.error(err);
+  //       //   console.log('Token stored to', TOKEN_PATH);
+  //       // });
+  //       callback(oAuth2Client);
+  //     });
+  //   });
+  // };
 
   handleAddDiary = () => {
     const { color } = this.state;
     const title = this.title.getText();
     const content = this.content.getText();
     const date = this.datePickerRef.getText();
-    const time = this.timePickerRef.getText();
-    console.log('payload', title, content, date, time, color);
+    const startTime = this.starttimePickerRef.getText();
+    const endTime = this.endTimePickerRef.getText();
+    console.log('payload', title, content, date, startTime, endTime, color);
+    const eventFromNow: object = {
+      summary: 'Poc Dev From Now',
+      time: 480,
+    };
+
+    RNCalendarEvents.findCalendars().then(
+      (result) => {
+        Alert.alert(
+          'Calendars',
+          result
+            .reduce((acc: any, cal: any) => {
+              acc.push(cal.title);
+              return acc;
+            }, [])
+            .join('\n'),
+        );
+      },
+      (result) => {
+        console.error(result);
+      },
+    );
     firestore()
       .collection('Diaries')
       .add({
         title,
         content,
         date,
-        time,
+        time: startTime,
         color,
         status: 0,
       })
@@ -79,7 +143,9 @@ class AddDiary extends PureComponent<Props, State> {
             marginTop={10}
             placeholder="Add a note"
           />
-          <Text color="#4a148c" fontSize={18}>Pick a day</Text>
+          <Text color="#4a148c" fontSize={18}>
+            Pick a day
+          </Text>
           <DateTimePicker
             height={50}
             marginTop={10}
@@ -92,46 +158,50 @@ class AddDiary extends PureComponent<Props, State> {
             placeholder="Pick a date"
             shadow
           />
-          <Text color="#4a148c" fontSize={18}>Begin time</Text>
+          <Text color="#4a148c" fontSize={18}>
+            Begin time
+          </Text>
           <DateTimePicker
             height={50}
             mode="time"
             shadow
             value={new Date()}
             ref={(ref) => {
-              this.timePickerRef = ref;
+              this.starttimePickerRef = ref;
             }}
           />
-          <Text color="#4a148c" fontSize={18}>End time</Text>
+          <Text color="#4a148c" fontSize={18}>
+            End time
+          </Text>
           <DateTimePicker
             height={50}
             mode="time"
             shadow
             value={new Date()}
             ref={(ref) => {
-              this.timePickerRef = ref;
+              this.endTimePickerRef = ref;
             }}
           />
           <QuickView height={400}>
             <ColorPicker
               onColorSelected={(color) =>
                 // Alert.alert(`Color selected: ${color}`)
-                this.setState({ color })}
+                this.setState({ color })
+              }
               style={{ flex: 1 }}
             />
           </QuickView>
-          
         </Body>
-        <Button 
-            center
-            width={400}
-            height={60}
-            title="Add task"
-            onPress={() => {
-              this.handleAddDiary();
-              NavigationService.goBack();
-            }}
-          />
+        <Button
+          center
+          width={400}
+          height={60}
+          title="Add task"
+          onPress={() => {
+            this.handleAddDiary();
+            NavigationService.goBack();
+          }}
+        />
       </Container>
     );
   }
